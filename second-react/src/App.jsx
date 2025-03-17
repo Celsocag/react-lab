0712b24cf34392a378b1ad99f3e5622a50de8d39
Celsocag <celsoalbugo@gmail.com>
2025-03-17 13:36:30 -0300

    Lesson about Absolute path to use assets

---
+++ second-react/src/App.jsx
@@ -0,0 +1,16 @@
+// Absolute path to use assets
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <main>
+          <img src="src/assets/react.svg" className="logo react" alt="React logo" />
+      </main>
+       
+    </>
+  )
+}
+
+export default App
