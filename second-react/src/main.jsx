97b71e7429839e3ceaa2257c9adbab4b65a2cc34
Celsocag <celsoalbugo@gmail.com>
2025-03-17 14:53:09 -0300

    Composable Apps

--- second-react/src/main.jsx
+++ second-react/src/main.jsx
@@ -1,23 +1,48 @@
-// Custom Components
+// Composable Apps
 
-import { createRoot } from "react-dom/client"
-const root = createRoot(document.getElementById("root"))
+import { createRoot } from "react-dom/client";
 
-function MainComponent() {
+const root = createRoot(document.getElementById("root"));
+
+function HeaderComponent() {
+  return (
+    <header>
+      <img src="react-logo.png" width="40px" alt="React logo" />
+    </header>
+  );
+}
+function MainConponent() {
   return (
     <main>
-      <h1>Fun facts about React!</h1>
-      <ul>
-          <li>Was first release in 2013</li>
-          <li>Was originally created by Jordan Walke</li>
-          <li>Has well over 200K stars on GitHub</li>
-          <li>Is maintained by Meta</li>
-          <li>Powers thousands of enterprise apps, including mobile apps</li>
-      </ul>
+      <h1>Reason I am excited to learn React</h1>
+      <ol>
+        <li>
+          React is a popular library, so I will be able to fit in with all the
+          coolest devs out there! 😎
+        </li>
+        <li>
+          I am more likely to get a job as a front end developer if I know React
+        </li>
+      </ol>
     </main>
-  )
+  );
+}
+function FooterComponent() {
+  return (
+    <footer>
+      <small>© 2024 Ziroll development. All rights reserved.</small>
+    </footer>
+  );
 }
 
-root.render(
-  <MainComponent/>
-)
+function MainPage() {
+  return (
+    <>
+      <HeaderComponent />
+      <MainConponent />
+      <FooterComponent />
+    </>
+  );
+}
+
+root.render(<MainPage />);
