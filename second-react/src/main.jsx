f8407f28f7339fe1c91879f8bad04ba4b950a09e
Celsocag <celsoalbugo@gmail.com>
2025-03-17 14:09:55 -0300

    Custom Components Lesson

--- second-react/src/main.jsx
+++ second-react/src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// Custom Components
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import { createRoot } from "react-dom/client"
+const root = createRoot(document.getElementById("root"))
+
+function MainComponent() {
+  return (
+    <main>
+      <h1>Fun facts about React!</h1>
+      <ul>
+          <li>Was first release in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Has well over 200K stars on GitHub</li>
+          <li>Is maintained by Meta</li>
+          <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
+  )
+}
+
+root.render(
+  <MainComponent/>
 )
