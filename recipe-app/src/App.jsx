2ac06b654d20541082f253a256145695477a3eb7
Celsocag <celsoalbugo@gmail.com>
2025-03-22 13:06:44 -0300

    Conditional Rendering

--- recipe-app/src/App.jsx
+++ recipe-app/src/App.jsx
@@ -1,11 +1,16 @@
 import Header from "./Header"
 import Main from "./Main"
+import Form from './Form';
 
 export default function App() {
   return (
     <>
-    <Header />
-      <Main />
+<Header />
+<Main />
+    
     </>
   )
 }
+
+{/* <Header />
+<Main /> */}
