7ee9005c03d06a5111181a81572fbf0beb66640b
Celsocag <celsoalbugo@gmail.com>
2025-03-27 20:47:11 -0300

    AI API

--- recipe-app/src/Main.jsx
+++ recipe-app/src/Main.jsx
@@ -1,47 +1,44 @@
-import React from "react";
-import Count from "./Count";
-import ClaudeRecipe from "./components/ClaudeRecipe";
-import IngredientList from "./components/IngredientList";
+import React from "react"
+import IngredientsList from "./components/IngredientsList"
+import ClaudeRecipe from "./components/ClaudeRecipe"
+import { getRecipeFromMistral } from "./ai"
 
 export default function Main() {
-  const [ingredients, setIngredients] = React.useState([
-    "all the main spices",
-    "pasta",
-    "ground beef",
-    "tomato paste",
-  ]);
-  const [recipeShown, setRecipeShown] = React.useState(false);
+    const [ingredients, setIngredients] = React.useState(
+        ["all the main spices", "pasta", "ground beef", "tomato paste"]
+    )
+    const [recipe, setRecipe] = React.useState("")
 
-  function toggleRecipeShown() {
-    setRecipeShown((prevShown) => !prevShown);
-  }
+    async function getRecipe() {
+        const recipeMarkdown = await getRecipeFromMistral(ingredients)
+        setRecipe(recipeMarkdown)
+    }
 
+    function addIngredient(formData) {
+        const newIngredient = formData.get("ingredient")
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient])
+    }
 
+    return (
+        <main>
+            <form action={addIngredient} className="add-ingredient-form">
+                <input
+                    type="text"
+                    placeholder="e.g. oregano"
+                    aria-label="Add ingredient"
+                    name="ingredient"
+                />
+                <button>Add ingredient</button>
+            </form>
 
-  function addIngredient(formData) {
-    const newIngredient = formData.get("ingredient");
-    setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
-  }
+            {ingredients.length > 0 &&
+                <IngredientsList
+                    ingredients={ingredients}
+                    getRecipe={getRecipe}
+                />
+            }
 
-  return (
-    <main>
-      <form action={addIngredient} className="add-ingredient-form">
-        <input
-          type="text"
-          placeholder="e.g. oregano"
-          aria-label="Add ingredient"
-          name="ingredient"
-        />
-        <button>Add ingredient</button>
-      </form>
-
-      {ingredients.length > 0 && (
-          <IngredientList
-             ingredients={ingredients}
-            toggleRecipeShown={toggleRecipeShown}
-          />
-      )}
-      {recipeShown ? <ClaudeRecipe/> : null}
-    </main>
-  );
+            {recipe && <ClaudeRecipe recipe={recipe} />}
+        </main>
+    )
 }
